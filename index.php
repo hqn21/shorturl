d4d1ea631149e6be58f7bac018c78c424110160b
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2022-02-18 20:42:42 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // MySQL 設定
+    $mysql = mysqli_connect('HOSTNAME', 'USERNAME', 'PASSWORD', 'DATABASE');
+    mysqli_query($mysql, "SET NAMES UTF8");
+
+    $mode = $_POST['mode'];
+
+    switch($mode) {
+
+        case 'sendData':
+            global $mysql;
+            $unique = strtolower($_POST['unique']);
+            $original = strtolower($_POST['original']);
+            $sendDataAction = mysqli_query($mysql, "INSERT INTO url (unique_id, original_url) VALUES ('$unique', '$original')");
+            if($sendDataAction) {
+                echo json_encode("OK!");
+            }
+            else {
+                echo json_encode("ERROR!");
+            }
+            break;
+
+        case 'deleteUniqueId':
+            global $mysql;
+            $unique = strtolower($_POST['unique']);
+            $deleteUniqueIdAction = mysqli_query($mysql, "DELETE FROM url WHERE unique_id = '$unique'");
+            if($deleteUniqueIdAction) {
+                echo json_encode("OK!");
+            }
+            else {
+                echo json_encode("ERROR!");
+            }
+            break;
+    }
+}
+else {
+    http_response_code(403);
+    die("You have no permission to access this page!");
+}
+?>
